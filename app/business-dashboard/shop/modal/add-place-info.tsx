1aa0207fb866b28bf61c1549d94eb90b19ac1e48
aclemente1988 <108532827+aclemente1988@users.noreply.github.com>
2025-01-16 23:36:54 +0100

    fixing bugs v8

--- app/business-dashboard/shop/modal/add-place-info.tsx
+++ app/business-dashboard/shop/modal/add-place-info.tsx
@@ -116,110 +116,131 @@ export function AddPlaceInfoModal({
         </DialogHeader>
 
         <div className="p-3 md:p-4 space-y-3 md:space-y-4">
-          <div className="space-y-3 px-4 md:px-8">
-            <div className="relative flex justify-center md:block">
-              <Input 
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="
-                  bg-main-gray 
-                  pl-14 
-                  h-[78px] 
-                  w-[390px]
-                  rounded-[100px]
-                  border-0
-                  mx-auto
-                  mr-[250px]
-                  md:mx-0
-                  md:h-[78px]
-                  md:w-[558px]
-                  md:rounded-[100px]
-                  text-third-gray
-                  max-md:text-[18px]
-                  max-md:font-['Open_Sans']
-                  max-md:font-semibold
-                  max-md:leading-[22px]
-                " 
-                placeholder="Branch name"
-              />
-              <svg
-                className="absolute left-8 top-1/2 -translate-y-1/2 h-4 w-4 text-[#7B7B7B]"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
-              </svg>
+          <div className="
+            space-y-3 
+            px-4 
+            md:px-8
+            -mt-14
+            md:mt-0
+          ">
+            <div>
+              <label className="block text-[16px] font-[700] leading-[26px] text-black mb-2 pl-8">
+                Branch Name
+              </label>
+              <div className="relative flex justify-center md:block">
+                <Input 
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="
+                    bg-main-gray 
+                    pl-14 
+                    h-[78px] 
+                    w-[390px]
+                    rounded-[100px]
+                    border-0
+                    mx-auto
+                    mr-[250px]
+                    md:mx-0
+                    md:h-[78px]
+                    md:w-[558px]
+                    md:rounded-[100px]
+                    text-third-gray
+                    max-md:text-[18px]
+                    max-md:font-['Open_Sans']
+                    max-md:font-semibold
+                    max-md:leading-[22px]
+                  " 
+                  placeholder="Branch name"
+                />
+                <svg
+                  className="absolute left-8 top-1/2 -translate-y-1/2 h-4 w-4 text-[#7B7B7B]"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"/>
+                </svg>
+              </div>
             </div>
 
-            <div className="relative flex justify-center md:block">
-              <Input 
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-                className="
-                  bg-main-gray 
-                  pl-14 
-                  h-[78px] 
-                  w-[390px]
-                  rounded-[100px]
-                  border-0
-                  mx-auto
-                  mr-[250px]
-                  md:mx-0
-                  md:h-[78px]
-                  md:w-[558px]
-                  md:rounded-[100px]
-                  text-third-gray
-                  max-md:text-[18px]
-                  max-md:font-['Open_Sans']
-                  max-md:font-semibold
-                  max-md:leading-[22px]
-                " 
-                placeholder="Branch address"
-              />
-              <svg
-                className="absolute left-8 top-1/2 -translate-y-1/2 h-4 w-4 text-[#7B7B7B]"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 2C8.134 2 5 5.134 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.866-3.134-7-7-7zm0 9a2 2 0 110-4 2 2 0 010 4z"/>
-              </svg>
+            <div>
+              <label className="block text-[16px] font-[700] leading-[26px] text-black mb-2 pl-8">
+                Address
+              </label>
+              <div className="relative flex justify-center md:block">
+                <Input 
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
+                  className="
+                    bg-main-gray 
+                    pl-14 
+                    h-[78px] 
+                    w-[390px]
+                    rounded-[100px]
+                    border-0
+                    mx-auto
+                    mr-[250px]
+                    md:mx-0
+                    md:h-[78px]
+                    md:w-[558px]
+                    md:rounded-[100px]
+                    text-third-gray
+                    max-md:text-[18px]
+                    max-md:font-['Open_Sans']
+                    max-md:font-semibold
+                    max-md:leading-[22px]
+                  " 
+                  placeholder="Branch address"
+                />
+                <svg
+                  className="absolute left-8 top-1/2 -translate-y-1/2 h-4 w-4 text-[#7B7B7B]"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2C8.134 2 5 5.134 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.866-3.134-7-7-7zm0 9a2 2 0 110-4 2 2 0 010 4z"/>
+                </svg>
+              </div>
             </div>
 
-            <div className="relative flex justify-center md:block">
-              <Input 
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                className="
-                  bg-main-gray 
-                  pl-14 
-                  h-[78px] 
-                  w-[390px]
-                  rounded-[100px]
-                  border-0
-                  mx-auto
-                  mr-[250px]
-                  md:mx-0
-                  md:h-[78px]
-                  md:w-[558px]
-                  md:rounded-[100px]
-                  text-third-gray
-                  max-md:text-[18px]
-                  max-md:font-['Open_Sans']
-                  max-md:font-semibold
-                  max-md:leading-[22px]
-                " 
-                placeholder="Phone number"
-              />
-              <svg
-                className="absolute left-8 top-1/2 -translate-y-1/2 h-4 w-4 text-[#7B7B7B]"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
-              </svg>
+            <div>
+              <label className="block text-[16px] font-[700] leading-[26px] text-black mb-2 pl-8">
+                Phone Number
+              </label>
+              <div className="relative flex justify-center md:block">
+                <Input 
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  className="
+                    bg-main-gray 
+                    pl-14 
+                    h-[78px] 
+                    w-[390px]
+                    rounded-[100px]
+                    border-0
+                    mx-auto
+                    mr-[250px]
+                    md:mx-0
+                    md:h-[78px]
+                    md:w-[558px]
+                    md:rounded-[100px]
+                    text-third-gray
+                    max-md:text-[18px]
+                    max-md:font-['Open_Sans']
+                    max-md:font-semibold
+                    max-md:leading-[22px]
+                  " 
+                  placeholder="Phone number"
+                />
+                <svg
+                  className="absolute left-8 top-1/2 -translate-y-1/2 h-4 w-4 text-[#7B7B7B]"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
