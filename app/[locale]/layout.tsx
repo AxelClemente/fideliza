f816ac3a749e7ee38009619c09618a4687dc0977
aclemente1988 <108532827+aclemente1988@users.noreply.github.com>
2025-01-31 13:44:37 +0100

    intl in progress landingpage v3

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -42,7 +42,8 @@ export default async function RootLayout({
   let messages;
   try {
     messages = (await import(`../../messages/${locale}.json`)).default;
-  } catch (_) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
     notFound();
   }
 
