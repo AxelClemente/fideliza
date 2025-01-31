6f8182b1f7ebf5c999ba62cf8166756ff4e7ad61
aclemente1988 <108532827+aclemente1988@users.noreply.github.com>
2025-01-31 13:42:05 +0100

    intl in progress landingpage v2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
   let messages;
   try {
     messages = (await import(`../../messages/${locale}.json`)).default;
-  } catch (error) {
+  } catch (_) {
     notFound();
   }
 
