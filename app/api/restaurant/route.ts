21ffc6c7281120f64215f9d5ae37d298bb8f6dbe
aclemente1988 <108532827+aclemente1988@users.noreply.github.com>
2025-02-22 21:52:22 +0100

    share link completed v1

--- app/api/restaurant/route.ts
+++ app/api/restaurant/route.ts
@@ -272,7 +272,7 @@ export async function DELETE(request: Request) {
   }
 }
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
     const session = await getServerSession(authOptions)
     if (!session?.user?.id) {
